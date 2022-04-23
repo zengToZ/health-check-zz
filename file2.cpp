4a1bd81ffe24aacfb1d8240ce5b1a14b1a4c6656
zengToZ <zengzhuang@gmail.com>
2022-04-23 11:16:08 -0700

    Fixed file2 conflicts

--- file2.cpp
+++ file2.cpp
@@ -5,6 +5,11 @@ void check_func(int args[]){
   std::vector<int> v;
   for(auto a : args){
     v.push_back(a);
+	// to print
     cout<<a<<endl;  
   }
+}
+
+int get_id(int key){
+  return key*100+ (int)key%2;
 }
